324b97e6333a42f08c6dd191122f4e946479753a
Cristian Garcia <cristiangarciatolavi+github@gmail.com>
2024-06-23 16:13:20 -0400

    Initial Commit

---
+++ CursoiOS/CursoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOSApp.swift
+//  CursoiOS
+//
+//  Created by Cristian Garcia on 23/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CursoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
